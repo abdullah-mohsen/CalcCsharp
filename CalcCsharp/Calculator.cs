2a8b260de6b05f0531071c30257c5cb7e64287c5
abdullah-mohsen <abdullahhaa144@gmail.com>
2016-07-19 09:05:11 +0300

    a

--- CalcCsharp/Calculator.cs
+++ CalcCsharp/Calculator.cs
@@ -13,9 +13,9 @@ namespace CalcCsharp
     public partial class Calculator : Form
     {
         //some Variable
-        string checkend = null;
+        string checkEnd = null;
         double number;
-        string op;
+        string opertion;
         public Calculator()
         {
             InitializeComponent();
@@ -27,75 +27,6 @@ namespace CalcCsharp
             textBox1.Clear();
         }
         // add the number of button in textbox
-        private void btnOne_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btntwo__Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnthree_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnfour__Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnfive_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnsix_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void btnseven_Click(object sender, EventArgs e)
-        {
-           
-        }
-
-        private void btnEight_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnNine_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void btnZero_Click(object sender, EventArgs e)
-        {
-            
-        }
-        // operation button
-        private void btnMinus_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void btnplus_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void btnmultiply_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void btndivide_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void btnEqual_Click(object sender, EventArgs e)
-        {
-        }
         public void DisplayNumber(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
@@ -107,17 +38,17 @@ namespace CalcCsharp
             {
                 if (textBox1.Text.Length > 0)
                 {
-                    checkend = textBox1.Text;
-                    checkend = checkend.Substring(textBox1.Text.Length - 1);
+                    checkEnd = textBox1.Text;
+                    checkEnd = checkEnd.Substring(textBox1.Text.Length - 1);
                     Button clickedButton = (Button)sender;
                     number = double.Parse(textBox1.Text);
-                    op = clickedButton.Text;
-                    if (checkend != clickedButton.Text)
+                    opertion = clickedButton.Text;
+                    if (checkEnd != clickedButton.Text)
                     {
                         textBox1.Text += clickedButton.Text;
                         textBox1.Text = "";
                     }
-                    else if (checkend == clickedButton.Text)
+                    else if (checkEnd == clickedButton.Text)
                     {
                        // textBox1.Text = "not click double opreation";
                         return;
@@ -125,7 +56,6 @@ namespace CalcCsharp
                     else
                     {
                         //textBox1.Text = check.Substring(0,check.Length-2) + op;
-
                     }
                       
                 }
@@ -136,8 +66,8 @@ namespace CalcCsharp
         }
         public void compute(object sender, EventArgs e)
         {
-            DisplayText.Text = number + " " + op + " "  +textBox1.Text;
-            switch (op)
+            DisplayText.Text = number + " " + opertion + " "  +textBox1.Text;
+            switch (opertion)
             {
                 case "+":
                     textBox1.Text = (number + double.Parse(textBox1.Text)).ToString();
